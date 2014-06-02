a3984280aebbd2cc51dd1a15a515f8530c643d31
Justin Coyne <justin@curationexperts.com>
2014-06-02 11:32:40 -0500

    Remove unused method from generator

--- sufia-models/lib/generators/sufia/models/install_generator.rb
+++ sufia-models/lib/generators/sufia/models/install_generator.rb
@@ -17,21 +17,6 @@ This generator makes the following changes to your application:
  5. Generates usage stats config
        """
 
-  # Implement the required interface for Rails::Generators::Migration.
-  # taken from http://github.com/rails/rails/blob/master/activerecord/lib/generators/active_record.rb
-  def self.next_migration_number(path)
-    if @prev_migration_nr
-      @prev_migration_nr += 1
-    else
-      if last_migration = Dir[File.join(path, '*.rb')].sort.last
-        @prev_migration_nr = last_migration.sub(File.join(path, '/'), '').to_i + 1
-      else
-        @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
-      end
-    end
-    @prev_migration_nr.to_s
-  end
-
   def banner
     say_status("warning", "GENERATING SUFIA MODELS", :yellow)
   end
