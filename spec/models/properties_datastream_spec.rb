2ed7914a1c6277a86cf419bb5bd9f216e4d049f5
Justin Coyne <justin@curationexperts.com>
2014-07-30 09:34:13 -0500

    Refactor properties_datastream_spec

--- spec/models/properties_datastream_spec.rb
+++ spec/models/properties_datastream_spec.rb
@@ -1,22 +1,26 @@
 require 'spec_helper'
 
 describe PropertiesDatastream do
+  let(:datastream) { PropertiesDatastream.new(double('base object', uri: '/test/foo', new_record?: true), 'properties') }
 
-  it "should have import_url" do
-    subject.import_url = 'http://example.com/somefile.txt'
-    subject.import_url.should == ['http://example.com/somefile.txt']
-    subject.ng_xml.to_xml.should be_equivalent_to "<?xml version=\"1.0\"?><fields><importUrl>http://example.com/somefile.txt</importUrl></fields>"
+  before do
+    datastream.import_url = 'http://example.com/somefile.txt'
+  end
+
+  describe "#import_url" do
+    subject { datastream.import_url }
+    it { should eq ['http://example.com/somefile.txt'] }
+  end
+
+  describe "#ng_xml" do
+    subject { datastream.ng_xml.to_xml }
+    it { should be_equivalent_to "<?xml version=\"1.0\"?><fields><importUrl>http://example.com/somefile.txt</importUrl></fields>" }
   end
 
   describe "to_solr" do
-    before do
-      @doc = PropertiesDatastream.new(nil, 'properties').tap do |ds|
-        ds.import_url = 'http://example.com/somefile.txt'
-      end
-    end
-    subject { @doc.to_solr}
+    subject { datastream.to_solr}
     it "should have import_url" do
-      subject['import_url_ssim'].should == ['http://example.com/somefile.txt']
+      expect(subject['import_url_ssim']).to eq ['http://example.com/somefile.txt']
     end
   end
 end
