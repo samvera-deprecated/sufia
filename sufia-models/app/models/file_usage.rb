074f11433eddfafa72ea6135db76e7563c9367c5
Justin Coyne <justin@curationexperts.com>
2014-11-01 21:50:22 -0500

    Update tests to show new permissions_attributes usage

--- sufia-models/app/models/file_usage.rb
+++ sufia-models/app/models/file_usage.rb
@@ -6,7 +6,7 @@ class FileUsage
     self.id = id
     self.path = Sufia::Engine.routes.url_helpers.generic_file_path(Sufia::Noid.noidify(id))
     earliest = Sufia.config.analytic_start_date
-    self.created = DateTime.parse(::GenericFile.find(id).create_date)
+    self.created = ::GenericFile.find(id).create_date
     self.created = earliest > created ? earliest : created unless earliest.blank?
     self.downloads = FileDownloadStat.to_flots FileDownloadStat.statistics(id, created)
     self.pageviews = FileViewStat.to_flots FileViewStat.statistics(id, created)
