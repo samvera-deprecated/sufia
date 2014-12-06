4545a7af8e04c8332f334571e5bc9b326933f720
Adam Wead <amsterdamos@gmail.com>
2014-12-06 11:59:42 -0500

    Run audits on ResourceVersion instead of uris
    This brings Sufia’s audit process inline with ActiveFedora’s updated
    version interface.

--- spec/models/generic_file/audit_spec.rb
+++ spec/models/generic_file/audit_spec.rb
@@ -17,15 +17,14 @@ describe "generic file audits" do
 
   context "force an audit on a file with two versions" do 
     specify "should return two log results" do
-      pending "should use new AF version interface"
       logs = f.audit(true)
       expect(logs.length).to eq(2)
     end
   end
 
-  context "force an audit on a specific version" do 
+  context "force an audit on a specific version" do
     specify "should return a single log result" do
-      log = f.audit_each(f.content.versions.first.uri, true)
+      log = f.audit_file("content", f.content.versions.first.uri)
       expect(log).to_not be_nil
     end
   end
