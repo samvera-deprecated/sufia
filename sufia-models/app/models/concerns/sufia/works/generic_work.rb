c8bf773b7d5f9c9ea727530d47374f17c2cc64f1
Tingting Jiang <tingtingjh@gmail.com>
2015-05-13 17:55:46 -0400

    Merge the change in generic work model

--- sufia-models/app/models/concerns/sufia/works/generic_work.rb
+++ sufia-models/app/models/concerns/sufia/works/generic_work.rb
@@ -5,11 +5,7 @@ module Sufia::Works
 
     include Sufia::Works::Work
     include Sufia::Works::CurationConcern::WithBasicMetadata
-    # TODO: Remove these items once the collection size(#1120) and 
-    # processing tickets(#1122) are closed.
-    include Sufia::GenericFile::Batches
-    include Sufia::GenericFile::Content
-    include Sufia::GenericFile::Metadata
+    include Sufia::Works::GenericWork::Metadata
     include Sufia::GenericFile::Permissions
   end
 end
