6933ae4497eb89f15258f07c6cfcf3a5b7f3f71b
Carolyn Cole <cam156@psu.edu>
2015-05-29 14:48:50 -0400

    Adding a coffescript test for testing the terms of service javascript and single use link javascript Updated the spec runner to not fail on multiples of 10 and to output the number of jasmine tests run on passing to make it more transparant how many jasmine tests are running.

--- spec/javascripts/jasmine_spec.rb
+++ spec/javascripts/jasmine_spec.rb
@@ -1,16 +1,27 @@
 require 'spec_helper'
 
+# Run the jasmine tests by running the jasmine:ci rake command and capturing the output
+#   The spec will fail if any jasmine tests fails
+#
+#   If you have a jasmine syntax error the test will fail since there will not be zero failures
+#
+#   If you add a new jasmine test by adding a file to spec/javascripts/*spec.js* make sure the
+#   number of test run increments or you may have a syntax error inside your jasmine test
+#
 describe "Jasmine" do
   it "expects all jasmine tests to pass" do
     load_rake_environment ["#{jasmine_path}/lib/jasmine/tasks/jasmine.rake"]
     jasmine_out = run_task 'jasmine:ci'
     unless jasmine_out.include?  "0 failures"
-      puts "************************  Jasmine Output *************\n\n"
+      puts "\n\n************************  Jasmine Output *************"
       puts jasmine_out
       puts "************************  Jasmine Output *************\n\n"
+    else
+      js_specs_count = Dir['spec/javascripts/**/*_spec.js*'].count
+      puts "#{jasmine_out.match(/\n(.+) specs/)[1]} jasmine specs run (in #{js_specs_count} jasmine test files)"
     end
     expect(jasmine_out).to include "0 failures"
-    expect(jasmine_out).to_not include "0 specs"
+    expect(jasmine_out).to_not include "\n0 specs"
   end
 
 end
