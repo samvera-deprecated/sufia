b2625e956efd98c9183d0c7d2407615df12a6c96
Nathan Rogers <nrogers@clevelandart.org>
2015-06-05 13:02:00 -0400

    Support collection size calculations using a Solr query instead of loading each file sequentially. This should greatly improve performance and has been tested on collections with thousands of GenericFiles effectively
    See https://gist.github.com/narogers/2687608229cfc63b7c2d for a demonstration of the performance differences.

--- sufia-models/app/services/sufia/generic_file_indexing_service.rb
+++ sufia-models/app/services/sufia/generic_file_indexing_service.rb
@@ -1,11 +1,14 @@
 module Sufia
   class GenericFileIndexingService < ActiveFedora::IndexingService
+    STORED_INTEGER = Solrizer::Descriptor.new(:integer, :stored)
+
     def generate_solr_document
       super.tap do |solr_doc|
         solr_doc[Solrizer.solr_name('label')] = object.label
         solr_doc[Solrizer.solr_name('file_format')] = object.file_format
         solr_doc[Solrizer.solr_name('file_format', :facetable)] = object.file_format
         solr_doc['all_text_timv'] = object.full_text.content
+        solr_doc[Solrizer.solr_name('file_size', STORED_INTEGER)] = object.content.size.to_i
       end
     end
   end
