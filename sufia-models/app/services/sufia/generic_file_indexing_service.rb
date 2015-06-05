8b1eac21448eff4f9bd75f19c7f543f2a4008c66
Nathan Rogers <nrogers@clevelandart.org>
2015-06-05 13:02:00 -0400

    Make direct query to Solr for the file size property which gets stored whenever you save a file. This speeds up the calculation time considerably and it might even more be efficient if you did it all on the Solr end using the stats module

--- sufia-models/app/services/sufia/generic_file_indexing_service.rb
+++ sufia-models/app/services/sufia/generic_file_indexing_service.rb
@@ -6,6 +6,9 @@ module Sufia
         solr_doc[Solrizer.solr_name('file_format')] = object.file_format
         solr_doc[Solrizer.solr_name('file_format', :facetable)] = object.file_format
         solr_doc['all_text_timv'] = object.full_text.content
+
+        int_store = Solrizer::Descriptor.new(:integer, :indexed, :stored);
+        solr_doc[Solrizer.solr_name('file_size', int_store)] = object.content.size.to_i
       end
     end
   end
