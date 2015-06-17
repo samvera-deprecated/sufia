615022196a94ee1fce18ef81de820325458b40f3
Justin Coyne <justin@curationexperts.com>
2015-06-17 09:26:20 -0500

    Remove unused test helpers

--- spec/support/locations.rb
+++ spec/support/locations.rb
@@ -1,28 +1,7 @@
 module Locations
-  def go_to_dashboard
-    visit '/dashboard'
-    # causes selenium to wait until text appears on the page
-    expect(page).to have_content('My Dashboard')
-  end
-
   def go_to_dashboard_files
     visit '/dashboard/files'
     expect(page).to have_selector('li.active', text: "My Files")
-  end
-
-  def go_to_dashboard_collections
-    visit '/dashboard/collections'
-    expect(page).to have_content('My Collections')
-  end
-
-  def go_to_dashboard_shares
-    visit '/dashboard/shares'
-    expect(page).to have_content('Files Shared with Me')
-  end
-
-  def go_to_dashboard_highlights
-    visit '/dashboard/highlights'
-    expect(page).to have_content('My Highlights')
   end
 
   def go_to_user_profile
