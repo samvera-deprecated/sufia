7b0a79a1210900dac06fd5a2a45865d033760260
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2015-08-03 13:35:27 -0700

    Unregistered users should have the ability to see file citations

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -9,7 +9,10 @@ describe Sufia::Ability, type: :model do
     it { is_expected.not_to be_able_to(:create, TinymceAsset) }
     it { is_expected.not_to be_able_to(:create, ContentBlock) }
     it { is_expected.not_to be_able_to(:update, ContentBlock) }
-    it { is_expected.to be_able_to(:read,  ContentBlock) }
+    it { is_expected.to be_able_to(:read, GenericFile) }
+    it { is_expected.to be_able_to(:stats, GenericFile) }
+    it { is_expected.to be_able_to(:citation, GenericFile) }
+    it { is_expected.to be_able_to(:read, ContentBlock) }
     it { is_expected.to be_able_to(:view_share_work, GenericFile) }
   end
 
