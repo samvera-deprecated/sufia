6a2a491e86e9641932d5a143f45ecd8f27d0aa7a
Adam Wead <amsterdamos@gmail.com>
2015-10-15 07:43:37 -0400

    Allow override of BatchController with customized BatchUpdateJob

--- app/controllers/concerns/sufia/batch_controller_behavior.rb
+++ app/controllers/concerns/sufia/batch_controller_behavior.rb
@@ -22,8 +22,7 @@ module Sufia
       @batch = Batch.find_or_create(params[:id])
       @batch.status = ["processing"]
       @batch.save
-      file_attributes = edit_form_class.model_attributes(params[:generic_file])
-      Sufia.queue.push(BatchUpdateJob.new(current_user.user_key, params[:id], params[:title], file_attributes, params[:visibility]))
+      Sufia.queue.push(batch_update_job)
       flash[:notice] = 'Your files are being processed by ' + t('sufia.product_name') + ' in the background. The metadata and access controls you specified are being applied. Files will be marked <span class="label label-danger" title="Private">Private</span> until this process is complete (shouldn\'t take too long, hang in there!). You may need to refresh your dashboard to see these updates.'
       if uploading_on_behalf_of? @batch
         redirect_to sufia.dashboard_shares_path
@@ -48,6 +47,15 @@ module Sufia
         file = batch.generic_files.first
         return false if file.nil? || file.on_behalf_of.blank?
         current_user.user_key != file.on_behalf_of
+      end
+
+      def batch_update_job
+        @batch_update_job ||= BatchUpdateJob.new(
+          current_user.user_key,
+          params[:id],
+          params[:title],
+          edit_form_class.model_attributes(params[:generic_file]),
+          params[:visibility])
       end
   end
 end
