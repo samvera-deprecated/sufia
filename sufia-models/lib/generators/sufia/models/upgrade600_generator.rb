3d1b7e6c762a84e13e4dd2be1d29b4e5aa0234ab
Colin Gross <grosscol@umich.edu>
2015-11-02 14:58:09 -0500

    Add dummy rails_helper to allow rspec to run. Refactor MimeType namespace to match current HydraWorks. Comment out upgrade specific migrations. Issue#97.

--- sufia-models/lib/generators/sufia/models/upgrade600_generator.rb
+++ sufia-models/lib/generators/sufia/models/upgrade600_generator.rb
@@ -11,7 +11,8 @@ This generator for upgrading sufia-models to 6.0 makes the following changes to 
   # Setup the database migrations
   def copy_migrations
     [
-      'change_audit_log_pid_to_generic_file_id.rb',
+      # Related to issue#97.  Migration is fails for install.
+      #'change_audit_log_pid_to_generic_file_id.rb',
       'change_proxy_deposit_request_pid_to_generic_file_id.rb'
     ].each do |file|
       better_migration_template file
