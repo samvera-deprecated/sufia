b4c51cda66fcbb8fc434f4cd60de7ee74847ece8
Aaron Collier <acollier@calstate.edu>
2016-10-28 15:42:28 -0700

    Fixing style issues

--- app/services/sufia/workflow/workflow_by_admin_set_strategy.rb
+++ app/services/sufia/workflow/workflow_by_admin_set_strategy.rb
@@ -1,20 +1,15 @@
 module Sufia
   module Workflow
     class WorkflowByAdminSetStrategy
-      def initialize(work, _attributes)
+      attr_reader :admin_set_id
+      def initialize(work, attributes)
         @work = work
-        unless _attributes[:admin_set_id].nil?
-            @admin_set_id = _attributes[:admin_set_id]
-        end
+        @admin_set_id = attributes[:admin_set_id] unless attributes[:admin_set_id].nil?
       end
 
       # @return [String] The name of the workflow by admin_set to use
       def work_name
         @work.model_name.singular
-      end
-
-      def admin_set_id
-        @admin_set_id
       end
     end
   end
