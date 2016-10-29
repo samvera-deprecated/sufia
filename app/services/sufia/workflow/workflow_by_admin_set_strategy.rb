48cc53dbe07452c2fdca87cc53647be3bde05de0
Aaron Collier <acollier@calstate.edu>
2016-10-28 19:12:50 -0700

    Editing per recommendations.

--- app/services/sufia/workflow/workflow_by_admin_set_strategy.rb
+++ app/services/sufia/workflow/workflow_by_admin_set_strategy.rb
@@ -4,12 +4,13 @@ module Sufia
       attr_reader :admin_set_id
       def initialize(work, attributes)
         @work = work
-        @admin_set_id = attributes[:admin_set_id] unless attributes[:admin_set_id].nil?
+        @admin_set_id = attributes[:admin_set_id]
       end
 
       # @return [String] The name of the workflow by admin_set to use
-      def work_name
-        @work.model_name.singular
+      def workflow_name
+        #@work.model_name.singular
+        'default'
       end
     end
   end
