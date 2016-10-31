227c34f81c54822d6ef64813b02683f1c7627128
Aaron Collier <acollier@calstate.edu>
2016-10-31 13:56:24 -0700

    Changes per @mjgiarlo requests.

--- app/services/sufia/workflow/workflow_by_admin_set_strategy.rb
+++ app/services/sufia/workflow/workflow_by_admin_set_strategy.rb
@@ -1,15 +1,14 @@
 module Sufia
   module Workflow
     class WorkflowByAdminSetStrategy
-      attr_reader :admin_set_id
       def initialize(work, attributes)
         @work = work
-        @admin_set_id = attributes[:admin_set_id]
+        @admin_set_id = attributes.fetch(:admin_set_id, 'default')
       end
 
       # @return [String] The name of the workflow by admin_set to use
       def workflow_name
-        @admin_set_id ||= 'default'
+        @admin_set_id
       end
     end
   end
