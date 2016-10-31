349552a3cf69115c5b5857202d156991a9bd73a3
Aaron Collier <acollier@calstate.edu>
2016-10-31 08:13:40 -0700

    Removing override of curation_concerns workflow_factory.

--- app/services/sufia/workflow/workflow_factory.rb
+++
@@ -1,7 +0,0 @@
-module Sufia
-  module Workflow
-    class WorkflowFactory < CurationConcerns::Workflow::WorkflowFactory
-      self.workflow_strategy = WorkflowByAdminSetStrategy
-    end
-  end
-end
