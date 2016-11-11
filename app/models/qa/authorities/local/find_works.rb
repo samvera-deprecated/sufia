f474e92afac4efd912c0e4e4bdba8bd98d0db2ce
Andy Smith <andjsmit@iupui.edu>
2016-11-11 13:37:12 -0500

    Remove test code from local QA.

--- app/models/qa/authorities/local/find_works.rb
+++ app/models/qa/authorities/local/find_works.rb
@@ -8,20 +8,8 @@ module Qa::Authorities
     end
 
     # TODO: Limit results to not self, editable, no existing parents, and no existing children
-    # Questions:
-    #  1. Should/can a Search Builder be used here?
-    #  2. How can the work ID and current user info be retrieved?
+
     def search(q, controller)
-
-=begin
-      results = []
-      ActiveFedora::SolrService.query("{!field f=title_tesim}#{q}", fl: [:id, :title_tesim]).each do |result|
-        id = result.fetch("id")
-        results.push(id: id, label: result.fetch("title_tesim").first, value: id)
-      end
-=end
-
-      puts "Controller: " + controller.object_id.to_s
 
       repo = CatalogController.new.repository
       builder = Sufia::FindWorksSearchBuilder.new(controller)
