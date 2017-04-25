388117d5f24e5607d3cb7d2810592df719c93993
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-04-25 09:16:26 -0700

    Add test coverage of default AS rake task

--- spec/tasks/rake_spec.rb
+++ spec/tasks/rake_spec.rb
@@ -12,6 +12,13 @@ describe "Rake tasks" do
     let!(:user1) { FactoryGirl.create(:user) }
     let!(:user2) { FactoryGirl.create(:user) }
 
+    before do
+      load_rake_environment [
+        File.expand_path("../../../tasks/sufia_user.rake", __FILE__),
+        File.expand_path("../../../lib/tasks/default_admin_set.rake", __FILE__)
+      ]
+    end
+
     it "creates a file" do
       run_task "sufia:user:list_emails"
       expect(File.exist?("user_emails.txt")).to be_truthy
@@ -25,6 +32,16 @@ describe "Rake tasks" do
       expect(File.exist?("abc123.txt")).to be_truthy
       expect(IO.read("abc123.txt")).to include(user1.email, user2.email)
       File.delete("abc123.txt")
+    end
+
+    describe 'sufia:default_admin_set:create' do
+      before do
+        AdminSet.find(AdminSet::DEFAULT_ID).eradicate if AdminSet.exists?(AdminSet::DEFAULT_ID)
+      end
+
+      it 'creates the default AdminSet' do
+        expect { run_task 'sufia:default_admin_set:create' }.to change { AdminSet.count }.by(1)
+      end
     end
   end
 
