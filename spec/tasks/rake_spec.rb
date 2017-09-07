50e650240659cedcb3d7ed34e42c8bad841c64af
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-09-07 08:28:40 -0700

    Align rake spec with same from master branch

--- spec/tasks/rake_spec.rb
+++ spec/tasks/rake_spec.rb
@@ -12,13 +12,6 @@ describe "Rake tasks" do
     let!(:user1) { FactoryGirl.create(:user) }
     let!(:user2) { FactoryGirl.create(:user) }
 
-    before do
-      load_rake_environment [
-        File.expand_path("../../../tasks/sufia_user.rake", __FILE__),
-        File.expand_path("../../../lib/tasks/default_admin_set.rake", __FILE__)
-      ]
-    end
-
     it "creates a file" do
       run_task "sufia:user:list_emails"
       expect(File.exist?("user_emails.txt")).to be_truthy
