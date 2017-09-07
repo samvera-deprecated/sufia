d700951a4b1572200a1e200ff8494a845f150817
Anna Headley <anna3LC@gmail.com>
2017-09-07 13:19:09 -0400

    Update file_uploader to stop overriding method defined in carrierwave
    backport from
    hyrax https://github.com/samvera/hyrax/commit/6ca1f779fe08311986c6a34fc3eecadbf0ed7f28

--- app/uploaders/sufia/uploaded_file_uploader.rb
+++ app/uploaders/sufia/uploaded_file_uploader.rb
@@ -3,20 +3,20 @@ module Sufia
     # Override the directory where uploaded files will be stored.
     # This is a sensible default for uploaders that are meant to be mounted:
     def store_dir
-      base_path + "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      configured_upload_path + "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
 
     def cache_dir
-      cache_path + "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      configured_cache_path + "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
 
     private
 
-      def base_path
+      def configured_upload_path
         Sufia.config.upload_path.call
       end
 
-      def cache_path
+      def configured_cache_path
         Sufia.config.cache_path.call
       end
   end
